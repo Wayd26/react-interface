d3cf38da187917c2fb8fcb76ed32698dde87523e
Wayd26 <wayedoun@gmail.com>
2022-09-13 09:03:24 +0100

    add Search and AddAppointment Components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
+import {BiCalendar} from 'react-icons/bi'
+import AddAppointment from './components/AddAppointment';
+import Search from './components/Search';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container mx-auto mt-3 font-thin">
+     <h1 className="text-5xl mb-3">
+      <BiCalendar className="inline-block text-red-400 align-top" /> Your appointments</h1>
+      <Search />
+      <AddAppointment />
     </div>
   );
 }
